93e27be6fd097f47b2de4ccf326051139c835f42
Eduardo Lobos <eduar92lobos@gmail.com>
2015-05-03 15:08:18 -0600

    inicie proyecto

---
+++ js/lobos.js
@@ -0,0 +1,10 @@
+
+ var get_tags = document.getElementsByTagName('footer'); 
+ var dos = get_tags[0].getElementsByTagName('p');
+ 
+ 
+ dos[0].innerHTML = 'Desarrollo de Aplicaciones Web -- ITCA-FEPADE 2015// Eduardo Lobos // Tatiana Iraheta'; 
+ dos[0].style.fontSize ='90%'; 
+ dos[0].style.color='#ccc';
+ dos[0].style.fontWeight =700;
+ dos[0].style.padding ='1px';
